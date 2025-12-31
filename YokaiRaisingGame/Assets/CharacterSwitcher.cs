a20fa74302845fa5b06bbaf9da02b77f220d5113
doramusuko2383 <doramusuko238@gmail.com>
2025-12-31 22:20:56 +0900

    Add character switcher for prefab swapping

---
+++ YokaiRaisingGame/Assets/CharacterSwitcher.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class CharacterSwitcher : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField] GameObject fireBallPrefab;
+    [SerializeField] GameObject yokaiChildPrefab;
+    [SerializeField] GameObject yokaiAdultPrefab;
+
+    GameObject currentInstance;
+
+    void Start()
+    {
+        SwitchTo(fireBallPrefab);
+    }
+
+    public void ShowFireBall()
+    {
+        SwitchTo(fireBallPrefab);
+    }
+
+    public void ShowYokaiChild()
+    {
+        SwitchTo(yokaiChildPrefab);
+    }
+
+    public void ShowYokaiAdult()
+    {
+        SwitchTo(yokaiAdultPrefab);
+    }
+
+    void SwitchTo(GameObject prefab)
+    {
+        if (prefab == null)
+        {
+            Debug.LogWarning("CharacterSwitcher: Prefab is not assigned.");
+            return;
+        }
+
+        if (currentInstance != null)
+        {
+            Destroy(currentInstance);
+        }
+
+        currentInstance = Instantiate(prefab, transform);
+        currentInstance.transform.localPosition = Vector3.zero;
+        currentInstance.transform.localRotation = Quaternion.identity;
+        currentInstance.transform.localScale = Vector3.one;
+    }
+}
