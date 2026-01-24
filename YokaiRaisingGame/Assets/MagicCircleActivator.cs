ba09e0eb39b5e6f142379e6f831ff718142de797
doramusuko2383 <doramusuko238@gmail.com>
2026-01-24 23:28:38 +0900

    Relax presentation dependencies and guard magic circle

--- YokaiRaisingGame/Assets/MagicCircleActivator.cs
+++ YokaiRaisingGame/Assets/MagicCircleActivator.cs
@@ -1,19 +1,45 @@
 using UnityEngine;
+using Yokai;
 
 public class MagicCircleActivator : MonoBehaviour
 {
     [SerializeField]
     GameObject magicCircleRoot;
 
+    [SerializeField]
+    YokaiStateController stateController;
+
+    bool hasWarnedMissingStateController;
+
     public void Show()
     {
+        if (!HasStateController())
+            return;
+
         if (magicCircleRoot != null)
             magicCircleRoot.SetActive(true);
     }
 
     public void Hide()
     {
+        if (!HasStateController())
+            return;
+
         if (magicCircleRoot != null)
             magicCircleRoot.SetActive(false);
+    }
+
+    bool HasStateController()
+    {
+        if (stateController != null)
+            return true;
+
+        if (!hasWarnedMissingStateController)
+        {
+            Debug.LogWarning("[MAGIC_CIRCLE] Missing StateController reference.");
+            hasWarnedMissingStateController = true;
+        }
+
+        return false;
     }
 }
